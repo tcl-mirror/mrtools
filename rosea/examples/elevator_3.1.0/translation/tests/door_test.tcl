28a1953b16dca94c88e766753d2bcca51f1bacb7
andrewm <andrewm>
2017-08-23 00:26:04 +0000

    Checkpoint on elevator translation.

--- rosea/examples/elevator_3.1.0/translation/tests/door_test.tcl
+++ rosea/examples/elevator_3.1.0/translation/tests/door_test.tcl
@@ -1,24 +1,50 @@
 source ./roseaTestUtil.tcl
 
+proc signalDoor {shaft signal} {
+    set door [roseaTestUtil sendCmd elevator::Door findById Shaft $shaft]
+    after 1000 [list roseaTestUtil sendCmd\
+        [list rosea tunnel $door signal $signal]]
+}
+
+proc simOpenDoor {domain class state} {
+    chan puts "opening door for $domain/$class/$state"
+    signalDoor S1 Door_opened
+}
+
+proc simCloseDoor {domain class state} {
+    chan puts "closing door for $domain/$class/$state"
+    signalDoor S1 Door_closed
+}
+
 roseaTestUtil call 50000
 
 roseaTestUtil sendCmd rosea trace control on
 roseaTestUtil sendCmd rosea trace control logon
 roseaTestUtil sendCmd rosea trace control loglevel info
 
+roseaTestUtil breakAtState elevator Door CLOSED
+roseaTestUtil breakAtState elevator Door OPENING simOpenDoor
+roseaTestUtil breakAtState elevator Door CLOSING simCloseDoor
+roseaTestUtil enableBreaks
+
+roseaTestUtil showClass elevator Shaft
+
 set door [roseaTestUtil sendCmd elevator::Door findById Shaft S1]
 roseaTestUtil sendCmd [list rosea tunnel $door signal Unlock]
+chan puts [roseaTestUtil waitForBreak]
 
-roseaTestUtil syncToState Door CLOSED
+roseaTestUtil breakAtState elevator Door OPEN_DELAY
 roseaTestUtil sendCmd [list rosea tunnel $door signal Passenger_open]
+chan puts [roseaTestUtil waitForBreak]
+roseaTestUtil deleteBreak elevator Door OPEN_DELAY
 
-roseaTestUtil syncToState Door OPEN_DELAY
 roseaTestUtil sendCmd [list rosea tunnel $door signal Passenger_close]
+chan puts [roseaTestUtil waitForBreak]
 
-roseaTestUtil syncToState Door CLOSED
 roseaTestUtil sendCmd [list rosea tunnel $door signal Unlock]
+chan puts [roseaTestUtil waitForBreak]
 
-roseaTestUtil syncToState Door CLOSED
+roseaTestUtil disableBreaks
 
 puts [roseaTestUtil sendCmd {rosea trace format [rosea trace decode all]}]
 
--- rosea/examples/elevator_3.1.0/translation/tests/roseaTestUtil.tcl
+++ rosea/examples/elevator_3.1.0/translation/tests/roseaTestUtil.tcl
@@ -40,52 +40,182 @@
 
 package require Tcl 8.6
 package require comm
+package require ral
+package require ralutil
 
 namespace eval ::roseaTestUtil {
-    namespace export call
-    namespace export sendCmd
-    namespace export syncToState
-    namespace ensemble create
-
     variable done 0
+    variable breakActions ; array set breakActions {}
 
     proc call {port} {
         variable remoteid $port
         comm::comm connect $remoteid
 
-        setupSync
+        setupRemote
     }
+    namespace export call
 
     proc sendCmd {args} {
         variable remoteid
         return [comm::comm send $remoteid {*}$args]
     }
+    namespace export sendCmd
 
-    proc setupSync {} {
-        sendCmd [list proc ::syncDone {actproc cmd code result op} {
-            trace remove execution $actproc leave [list ::syncDone $actproc]
+    proc setupRemote {} {
+        sendCmd [list namespace eval ::rosea::remote {
+            variable stateBreaks ; array set stateBreaks {}
 
-            set remoteid [lindex [comm::comm interps] end]
-            comm::comm send $remoteid [list set ::roseaTestUtil::done $code]
-        }]
+            variable areBreaksEnabled false
 
-        sendCmd [list proc ::syncToState {domain class state} {
-            set actproc [rosea info statemodel activityproc $domain $class\
-                    $state]
-            trace add execution $actproc leave [list ::syncDone $actproc]
+            proc defineStateBreak {domain class state} {
+                variable stateBreaks
+                variable areBreaksEnabled
+
+                set brkid $domain,$class,$state
+                if {![info exists stateBreaks($brkid)]} {
+                    set actproc [rosea info statemodel activityproc\
+                            $domain $class $state]
+                    set stateBreaks($brkid) $actproc
+
+                    if {$areBreaksEnabled} {
+                        trace add execution $actproc leave [list\
+                            ::rosea::remote::stateBreakHit $brkid]
+                    }
+                }
+            }
+            namespace export defineStateBreak
+
+            proc deleteStateBreak {domain class state} {
+                variable stateBreaks
+                variable areBreaksEnabled
+
+                set brkid $domain,$class,$state
+                if {[info exists stateBreaks($brkid)]} {
+                    if {$areBreaksEnabled} {
+                        set actproc $stateBreaks($brkid)
+                        trace remove execution $actproc leave [list\
+                            ::rosea::remote::stateBreakHit $brkid]
+                    }
+                    unset stateBreaks($brkid)
+                }
+            }
+            namespace export deleteStateBreak
+
+            proc enableStateBreaks {} {
+                variable stateBreaks
+                variable areBreaksEnabled true
+
+                foreach brkid [array names stateBreaks] {
+                    set actproc $stateBreaks($brkid)
+                    trace add execution $actproc leave [list\
+                        ::rosea::remote::stateBreakHit $brkid]
+                }
+            }
+            namespace export enableStateBreaks
+
+            proc disableStateBreaks {} {
+                variable stateBreaks
+                variable areBreaksEnabled false
+
+                foreach brkid [array names stateBreaks] {
+                    set actproc $stateBreaks($brkid)
+                    trace remove execution $actproc leave [list\
+                        ::rosea::remote::stateBreakHit $brkid]
+                }
+            }
+            namespace export disableStateBreaks
+
+            namespace ensemble create
+
+            proc stateBreakHit {brkid cmd code result op} {
+                variable stateBreaks
+
+                set remoteid [lindex [comm::comm interps] end]
+                lassign [split $brkid ,] domain class state
+                comm::comm send $remoteid [list set ::roseaTestUtil::done\
+                    [dict create Code $code Domain $domain Class $class\
+                        State $state]]
+            }
         }]
     }
 
-    proc syncToState {class state {timeout 20000}} {
-        sendCmd syncToState elevator $class $state
-        set timer [after $timeout set ::done TIMEOUT]
-        vwait [namespace current]::done
-        after cancel $timer
-
-        variable done
-        if {$done eq "TIMEOUT"} {
-            error "timed out while waiting to arrive in $class.$state"
+    proc breakAtState {domain class state {action {}}} {
+        sendCmd [list ::rosea::remote defineStateBreak $domain $class $state]
+        if {$action ne {}} {
+            variable breakActions
+            set breakActions($domain,$class,$state) $action
         }
-        return $done
     }
+    namespace export breakAtState
+
+    proc deleteBreak {domain class state} {
+        sendCmd [list ::rosea::remote deleteStateBreak $domain $class $state]
+        variable breakActions
+        unset -nocomplain breakActions($domain,$class,$state)
+    }
+    namespace export deleteBreak
+
+    proc enableBreaks {} {
+        sendCmd [list ::rosea::remote enableStateBreaks]
+    }
+    namespace export enableBreaks
+
+    proc disableBreaks {} {
+        sendCmd [list ::rosea::remote disableStateBreaks]
+    }
+    namespace export disableBreaks
+
+    proc waitForBreak {{timeout 0}} {
+        for {set waiting true} {$waiting} {} {
+            if {$timeout > 0} {
+                set timer [after $timeout set [namespace current]::done TIMEOUT]
+            } else {
+                set timer {}
+            }
+
+            vwait [namespace current]::done
+
+            if {$timer ne {}} {
+                after cancel $timer
+            }
+
+            set result [set [namespace current]::done]
+            if {$result eq "TIMEOUT"} {
+                error "timed out on state break point"
+            } elseif {[dict get $result Code] != 0} {
+                error "state activity for [dict get $result Domain]\
+                    [dict get $result Class] / [dict get $result State]\
+                    had result code \"[dict get $result Code]\""
+            } else {
+                dict with result {
+                    set brkid $Domain,$Class,$State
+                    variable breakActions
+                    if {[info exist breakActions($brkid)]} {
+                        try {
+                            set breakCmd $breakActions($brkid)
+                            lappend breakCmd $Domain $Class $State
+                            eval $breakCmd
+                        } on error {result opts} {
+                            chan puts "error executing \"$breakCmd\""
+                            return -options $opts $result
+                            set waiting false
+                        }
+                    } else {
+                        set waiting false
+                    }
+                }
+            }
+        }
+
+        return $result
+    }
+    namespace export waitForBreak
+
+    proc showClass {domain class} {
+        set insts [sendCmd ::rosea::InstCmds::deRef "\[::${domain}::${class} findAll\]"]
+        chan puts [ral relformat $insts]
+    }
+    namespace export showClass
+
+    namespace ensemble create
 }
