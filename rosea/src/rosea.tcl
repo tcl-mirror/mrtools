856ea6f1e043652c6cf5db1fef011299a915e7d3
andrewm <andrewm>
2017-10-26 00:28:16 +0000

    Release of rosea 1.8. Added command to obtain the id class for multi-assigner.

--- rosea/src/rosea.tcl
+++ rosea/src/rosea.tcl
@@ -77,7 +77,7 @@ namespace eval ::rosea {
     
     namespace ensemble create
 
-    variable version 1.7
+    variable version 1.8
 
     logger::initNamespace [namespace current]
 
@@ -1861,6 +1861,26 @@ namespace eval ::rosea {
             lassign [FindRelatedInsts $instref semiminus {*}$rchain] relvar insts
             tailcall ToRef $relvar [uplevel 1\
                 [list ::ral relation restrictwith $insts $expr]]
+        }
+        proc idInstance {instref} {
+            lassign $instref relvar insts
+            SplitRelvarName $relvar domain relationship
+            set assignrelvar ${domain}::${relationship}__STATEINST
+            if {![relvar exists $assignrelvar]} {
+                tailcall DeclError NO_ASSIGNER $rname
+            }
+        
+            set idclass [pipe {
+                relvar restrictone [namespace parent]::Config::MultipleAssigner\
+                        Domain [string trim $domain :] Relationship $relationship |
+                relation extract ~ Class
+            }]
+        
+            return [pipe {
+                relvar set ${domain}::$idclass |
+                relation semijoin $insts ~ |
+                ToRef ${domain}::$idclass ~
+            }]
         }
         proc updateAttribute {instref args} {
             if {[llength $args] % 2 != 0} {
